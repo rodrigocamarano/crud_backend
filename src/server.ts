16580d812283082f4974123a4f4e4c6098ecc976
Rodrigo Camarano <rodrigocamarano@gmail.com>
2022-11-07 14:39:25 -0300

    install and config compression, cors and helmet packages

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
 
 const server = express();
+server.use(helmet());
+server.use(compression());
+server.use(cors());
 server.use(express.json());
 
 server.get('/', (request, response) => response.json({ status: 'Ok' }));
